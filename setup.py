ef5ac67c8c158b04449320ee7031dfadd16f836b
fenilmaisuria <fenilmaisuria>
2023-01-20 10:12:40 +0000

    add file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "rf",
+    packages = ['rf'],
+    version = "0.0.2",
+    description = "Python package for Reconciliation Framework",
+    url="https://github.com/exocube-inc/Reconciliation-Framework-Package",
+    author = "Exocube"
+    )
